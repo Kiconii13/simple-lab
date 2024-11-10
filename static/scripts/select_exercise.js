54368085d7c0d7e83b31eaf861de0d73cccb303d
Despot Maksimovic <despotmaksimovic@gmail.com>
2024-11-10 12:34:07 +0100

    m

--- static/scripts/select_exercise.js
+++ static/scripts/select_exercise.js
@@ -22,6 +22,31 @@ const select = document.getElementById("year");
             });
 
             buttons.appendChild(btn);
+
+            buttons.innerHTML = "";
+
+            btn = document.createElement("input");
+            btn.type = "submit";
+            btn.classList.add("exercise_button");
+            btn.value = "Determining the Elastic Coefficient of a Spring";
+            btn.addEventListener("click", () => {
+              alert("Coming soon! :)");
+            });
+
+            buttons.appendChild(btn);
+
+            buttons.innerHTML = "";
+
+            btn = document.createElement("input");
+            btn.type = "submit";
+            btn.classList.add("exercise_button");
+            btn.value = "Determining the Equivalent Elastic Coefficient of Springs in Series and Parallel";
+            btn.addEventListener("click", () => {
+              alert("Coming soon! :)");
+            });
+
+            buttons.appendChild(btn);
+
             break;
           case "7":
             buttons.innerHTML = "";
@@ -55,6 +80,28 @@ const select = document.getElementById("year");
             btn.value = "Determining the equivalent resistance of\nseries and parallel connection of resistors";
             btn.addEventListener("click", () => {
               window.location.replace("/templates/e_0801/e_0801.html");
+            });
+
+            buttons.appendChild(btn);
+            buttons.innerHTML = "";
+
+            btn = document.createElement("input");
+            btn.type = "submit";
+            btn.classList.add("exercise_button");
+            btn.value = "Measuring the Period of a Pendulum's Oscillatior";
+            btn.addEventListener("click", () => {
+              alert("Coming soon! :)");
+            });
+
+            buttons.appendChild(btn);
+            buttons.innerHTML = "";
+
+            btn = document.createElement("input");
+            btn.type = "submit";
+            btn.classList.add("exercise_button");
+            btn.value = "Verifying the Law of Reflection Using a Plane Mirror";
+            btn.addEventListener("click", () => {
+              alert("Coming soon! :)");
             });
 
             buttons.appendChild(btn);
